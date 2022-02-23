8822cfea1bc3919b1bb6aac06e3e8656be93b6dc
Zackery Arsement <zarsement@hotmail.com>
2022-02-22 20:46:15 -0600

    Simulation can run a basic game and keep track of each teams final score and total shot attempts

--- simulation.js
+++ simulation.js
@@ -1,7 +1,7 @@
 // Golden State Warriors 2015-2016
 let goldenState = [
     {
-        player: "Steph Curry",
+        name: "Steph Curry",
         att2: 9,
         att3: 11.2,
         made2: 5.1,
@@ -17,7 +17,7 @@ let goldenState = [
         chanceDefReb: null
     },
     {
-        player: "Draymond Green",
+        name: "Draymond Green",
         att2: 6.9,
         att3: 3.2,
         made2: 3.7,
@@ -33,7 +33,7 @@ let goldenState = [
         chanceDefReb: null
     },
     {
-        player: "Klay Thompson",
+        name: "Klay Thompson",
         att2: 9.2,
         att3: 8.1,
         made2: 4.7,
@@ -49,7 +49,7 @@ let goldenState = [
         chanceDefReb: null
     },
     {
-        player: "Andre Iguodala",
+        name: "Andre Iguodala",
         att2: 3.3,
         att3: 2.4,
         made2: 1.9,
@@ -65,7 +65,7 @@ let goldenState = [
         chanceDefReb: null
     },
     {
-        player: "Harrison Barnes",
+        name: "Harrison Barnes",
         att2: 6.3,
         att3: 3.2,
         made2: 3.2,
@@ -93,7 +93,7 @@ let goldenState = [
 // Cleveland Caveliers 2015-2016
 let clevelandCaveliers = [
     {
-        player: "Lebron James",
+        name: "Lebron James",
         att2: 14.9,
         att3: 3.7,
         made2: 8.6,
@@ -109,7 +109,7 @@ let clevelandCaveliers = [
         chanceDefReb: null
     },
     {
-        player: "Kyrie Irving",
+        name: "Kyrie Irving",
         att2: 11.6,
         att3: 4.9,
         made2: 5.8,
@@ -125,7 +125,7 @@ let clevelandCaveliers = [
         chanceDefReb: null
     },
     {
-        player: "Kevin Love",
+        name: "Kevin Love",
         att2: 7,
         att3: 5.7,
         made2: 3.3,
@@ -141,7 +141,7 @@ let clevelandCaveliers = [
         chanceDefReb: null
     },
     {
-        player: "J.R. Smith",
+        name: "J.R. Smith",
         att2: 4.4,
         att3: 6.6,
         made2: 1.9,
@@ -157,7 +157,7 @@ let clevelandCaveliers = [
         chanceDefReb: null
     },
     {
-        player: "Tristan Thompson",
+        name: "Tristan Thompson",
         att2: 5.1,
         att3: 0,
         made2: 3,
@@ -185,7 +185,8 @@ let clevelandCaveliers = [
 teamSeasonPercentage(goldenState);
 teamSeasonPercentage(clevelandCaveliers);
 
-runGame(goldenState, clevelandCaveliers, 1, 2, 3);
+// Run a game where each team shoots at minimum 1 shot per 1 minute... 3 chances to put up missed shots
+runGame(goldenState, clevelandCaveliers, 1, 1, 3);
 
 // Establish each players 2pt% and 3pt% 
 function teamSeasonPercentage(currentTeam){
@@ -254,25 +255,8 @@ function playerGamePercentage(team1, team2){
     team2[team2.length-1].offRebPer = Math.round(team2[team2.length-1].offRebPer * 1000)/1000;
 }
 
-// Run a game simulation (team1, team2, team shots/iteration, minutes/iteration, reshots allowed / iteration)
-function runGame(homeTeam, visitorTeam, itShots, itMin, reshots){
-    let numbIterations = 48/itMin;
-
-    let homeTeamScore;
-    let visitorTeamScore;
-
-    // Get team percentages based on their opponent... chance to get rebounds
-    playerGamePercentage(homeTeam, visitorTeam)
-
-    for(let i=0; i<numbIterations; i++){
-        // Each team shoots once
-    }
-};
-
-teamShoots(clevelandCaveliers);
-
 // Figure out which player is shooting on the team
-function teamShoots(currentTeam){
+function teamShooter(currentTeam){
     // Pick a random number 0 and 1000. Each players will cover a range of numbers between 1 and 1000 proportional to there chance to shoot
     let randNumb = Math.floor(Math.random() * 1000);
     let deciderArray = [];
@@ -289,27 +273,172 @@ function teamShoots(currentTeam){
     const shooter = () => {
         switch (true) {
             case (randNumb <= deciderArray[0]):
-                curShoot = currentTeam[0].player;
+                curShoot = currentTeam[0];
                 return curShoot;
 
             case (randNumb <= deciderArray[1]):
-                curShoot = currentTeam[1].player;
+                curShoot = currentTeam[1];
                 return curShoot;
 
             case (randNumb <= deciderArray[2]):
-                curShoot = currentTeam[2].player;
+                curShoot = currentTeam[2];
                 return curShoot;
 
             case (randNumb <= deciderArray[3]):
-                curShoot = currentTeam[3].player;
+                curShoot = currentTeam[3];
                 return curShoot;
 
             case (randNumb <= deciderArray[4]):
-                curShoot = currentTeam[4].player;
+                curShoot = currentTeam[4];
                 return curShoot;
             }
 
         return curShoot;
     }
-    console.log(shooter() + " is shooting!");
+
+    return shooter();
 }
+
+// Figure out if the player shoots a 2pt or 3 pt... does he make it or miss
+function playerShoots(currentPlayer){
+    let randNumb = Math.floor(Math.random() * 1000);
+    let oddsTry2 = currentPlayer.chance2*1000;
+
+    // Shoots a 2pt
+    if(randNumb <= oddsTry2){
+        // Get another random number to see if the player makes the shot
+        randNumb = Math.floor(Math.random() * 1000);
+
+        let oddMake2 = currentPlayer.per2*1000;
+
+        // Player makes the shot
+        if(randNumb <= oddMake2){
+            return [2, true];
+        }
+        else{
+            return [2, false];
+        }
+    }
+    // Shoots a 3pt
+    else{
+        // Get another random number to see if the player makes the shot
+        randNumb = Math.floor(Math.random() * 1000);
+
+        let oddMake3 = currentPlayer.per3*1000;
+
+        // Player makes the shot
+        if(randNumb <= oddMake3){
+            return [3, true];
+        }
+        else{
+            return [3, false];
+        }
+    }
+}
+
+// Check which team gets the rebound
+function teamRebound(offTeam, defTeam){
+    let randNumb = Math.floor(Math.random() * 1000);
+
+    // Odds that the offensive team gets the rebound
+    let offChance = offTeam.offRebPer * 1000;
+
+    if(randNumb <= offChance){
+        return [offTeam, 'Offensive'];
+    }
+    else{
+        return [defTeam, 'Defensive'];
+    }
+}
+
+// Run a game simulation (team1, team2, team shots/iteration, minutes/iteration, reshots allowed / iteration)
+function runGame(homeTeam, visitorTeam, itShots, itMin, allowedReshots){
+    let numbIterations = 48/itMin;
+
+    let homeTeamScore = 0;
+    let visitorTeamScore = 0;
+
+    let homeTeamShots = 0;
+    let visitorTeamShots = 0;
+
+    // Get team percentages based on their opponent... chance to get rebounds
+    playerGamePercentage(homeTeam, visitorTeam)
+
+    for(let i=0; i<numbIterations; i++){
+        // Each team shoots at a base amount once... figure out which player shoots
+        let shooterHome = teamShooter(homeTeam);
+        let shooterVis = teamShooter(visitorTeam);
+
+        // See if that shooter tries a 2pt or 3pt... do they make the shot?
+        let homeShot = playerShoots(shooterHome);
+        let visitorShot = playerShoots(shooterVis);
+
+        // When a shot is missed teams can rebound and reshoot... limit is dependant on allowedReshots
+        let reshots = 0;
+
+        checkShotRedo('Home', homeShot, reshots);
+        checkShotRedo('Visitor', visitorShot, reshots);
+    }
+    
+    // Add to score if the shot it made, if it's missed then rebound and shoot again (Keep inside game function so scores can remain local variables)
+    function checkShotRedo(team, shotStatus, reshots){
+        // This function can only be ran dependant on the allowed reshots
+        if(reshots < allowedReshots){
+            // Count how many rebounded shots the team makes
+            reshots = reshots + 1;
+
+            // Home team shot
+            if(team === 'Home'){
+                if(shotStatus[1]){
+                    homeTeamScore = homeTeamScore + shotStatus[0];
+                } 
+                // If the team misses the shot then check to see what team gets the rebound
+                else{
+                    let reboundTeam = teamRebound(homeTeam, visitorTeam);
+                    
+                    // Whichever team gets the rebound gets to shoot the ball again
+                    let teamShot = playerShoots(teamShooter(reboundTeam[0]));
+
+                    // If the offensive team gets the rebound then home team can shoot
+                    if(reboundTeam[1] === 'Offensive'){
+                        checkShotRedo('Home', teamShot, reshots);
+                    }
+                    // Else then the defense can shoot
+                    else{
+                        checkShotRedo('Visitor', teamShot, reshots);
+                    }
+                }
+                homeTeamShots = homeTeamShots + 1;
+            } 
+            // Visitor team shot
+            else {
+                if(shotStatus[1]){
+                    visitorTeamScore = visitorTeamScore + shotStatus[0];
+                } 
+                // If the team misses the shot then check to see what team gets the rebound
+                else{
+                    let reboundTeam = teamRebound(visitorTeam, homeTeam);
+                    
+                    // Whichever team gets the rebound gets to shoot the ball again
+                    let teamShot = playerShoots(teamShooter(reboundTeam[0]));
+
+                    // If the offensive team gets the rebound then home team can shoot
+                    if(reboundTeam[1] === 'Offensive'){
+                        checkShotRedo('Visitor', teamShot, reshots);
+                    }
+                    // Else then the defense can shoot
+                    else{
+                        checkShotRedo('Home', teamShot, reshots);
+                    }
+                }
+
+                visitorTeamShots = visitorTeamShots + 1;
+            }
+        }
+    }
+
+    console.log('The home team scored ' + homeTeamScore + ' points!');
+    console.log('The visitor team scored ' + visitorTeamScore + ' points!');
+    console.log('The home team shot ' + homeTeamShots + ' times.');
+    console.log('The home team shot ' + visitorTeamShots + ' times.');
+};
